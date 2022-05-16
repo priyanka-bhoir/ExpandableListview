87a496ded035f8437828902163d52eec6838b804
priyanka-bhoir <er.priyankabhoir1@gmail.com>
2022-05-16 11:52:39 +0530

    initial

---
+++ app/src/main/java/com/priyanka/expandablelistview/GroupedItem.kt
@@ -0,0 +1,18 @@
+package com.priyanka.expandablelistview
+
+class GroupedItem(val header: Header,
+                  val itemList: List<Item>) {
+
+    class Header(
+       val  headerText: String
+    )
+
+    class Item(
+        val itemText : String
+    )
+
+
+
+
+
+}
---
+++ app/src/main/java/com/priyanka/expandablelistview/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.priyanka.expandablelistview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.ConcatAdapter
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+
+        val arrayList = arrayListOf<ExpandableAdapter>()
+
+        val groupedItemList = Repository.createGroupedItemList(5)
+
+        for(groupedItem in groupedItemList){
+            val adapter = ExpandableAdapter(groupedItem)
+            arrayList.add(adapter)
+        }
+
+        val concatAdapterConfig = ConcatAdapter.Config.Builder()
+            .setIsolateViewTypes(false)
+            .build()
+
+        val concatAdapter = ConcatAdapter(concatAdapterConfig, arrayList)
+
+        recyclerView.layoutManager = LinearLayoutManager(baseContext)
+        recyclerView.adapter = concatAdapter
+
+    }
+}
